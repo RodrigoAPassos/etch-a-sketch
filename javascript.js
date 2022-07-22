a347dbc23af28afa508c6c0fcbea7d126c1f7e52
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-07-22 19:11:41 -0300

    Add clear, rainbow and default buttons
    Add new functions and started to add style.
    
    Next add the shading option and finish the style.

--- javascript.js
+++ javascript.js
@@ -40,25 +40,90 @@ function clearGrid() {
     })
 }
 
-// --paint a sqr--
-function paint (sqr) {
-    sqr.classList.add("painted");
-}
-
-//createGrid(64);
 // --Mouse over--
 function mouseOver () {
     document.querySelectorAll(".square").forEach((sqr) => {
         sqr.addEventListener('mouseover', () => {
-            paint(sqr);
-            console.log("chamado")
+            sqr.setAttribute("style", "background: white; opacity: 1")
         })
     })
 }
 
+// --paint rainbow--
+function rainbow () {
+    document.querySelectorAll(".square").forEach((sqr) => {
+        sqr.addEventListener("mouseover", () => {
+            sqr.setAttribute("style", `background: rgb(${rgbColor()}); opacity: 1`)})
+        })
+}
+
+// --shading func--
+function shading() {
+    document.querySelectorAll(".square").forEach((sqr) => {
+        sqr.addEventListener("mouseover", () => {
+            sqr.setAttribute("style", `background: rgba(0, 0, 0, ${opacity()})`);
+        })
+    })
+}
+
+// --generate random integer 0-255--
+function randomInteger(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+// --generate random rgb color--
+function rgbColor () {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r, g, b];
+}
+
+// --opacity--
+/*function opacity() {
+    let opct = 0.1;
+    document.querySelectorAll(".square").forEach((sqr) => {
+        opct = window.getComputedStyle(sqr).getPropertyValue("opacity");
+        //console.log(opct);
+    })
+    //console.log(opct);
+    return (opct += 0.1);
+}*/
+
 // --Button for grid size--
 document.querySelector(".gridSize").addEventListener('click', () => {
+    let gSize = document.querySelector(".gridSize").innerHTML;
+    if (gSize == "Start") {
+        document.querySelector(".gridSize").innerHTML = "Grid Size";
+    }
+    document.querySelector(".defaultMode").classList.remove("on");
+    document.querySelector(".rainbowMode").classList.remove("on");
     clearGrid();
     defineSize();
+})
+
+// --button for rainbow mode--
+document.querySelector(".rainbowMode").addEventListener("click", () => {
+    document.querySelector(".rainbowMode").classList.add("on");
+    document.querySelector(".defaultMode").classList.remove("on");
+    rainbow ();
+})
+
+// --button for default mode--
+document.querySelector(".defaultMode").addEventListener("click", () => {
+    document.querySelector(".defaultMode").classList.add("on");
+    document.querySelector(".rainbowMode").classList.remove("on");
     mouseOver();
 })
+
+// --button for clear--
+document.querySelector(".clear").addEventListener("click", () => {
+    document.querySelectorAll(".square").forEach((sqr) => {
+        sqr.setAttribute("style", "background: skyblue")
+    })
+})
+
+// --button for shading--
+/*document.querySelector(".shading").addEventListener("click", () => {
+    shading();
+})*/
